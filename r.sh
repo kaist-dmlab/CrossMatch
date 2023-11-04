6b6582bbcf4b79a8af403bc3491bef638e0fe8fb
Yooju Shin <yooju24@gmail.com>
2023-11-04 11:06:50 -0700

    Update README.md

---
+++ r.sh
@@ -0,0 +1,7 @@
+sed -i -e '$a\ ' README_.md
+git add *
+git commit -m "Update README.md"
+git push
+git config credential.helper store
+git config credential.helper cache
+git config credential.helper 'cache --timeout=180000'
